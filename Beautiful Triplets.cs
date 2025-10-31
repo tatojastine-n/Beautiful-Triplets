c43d1d665e3d61e1dc45f34daa45bf3ac80974c7
tatojastine-n <jajanicoletato@gmail.com>
2025-10-31 15:25:56 +0800

    Beautiful Triplets.cs

---
+++ Beautiful Triplets.cs
@@ -0,0 +1,69 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'beautifulTriplets' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int beautifulTriplets(int d, List<int> arr)
+    {
+        int count = 0;
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            int first = arr[i];
+            int second = first + d;
+            int third = first + 2 * d;
+
+            if (arr.Contains(second) && arr.Contains(third))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.beautifulTriplets(d, arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
